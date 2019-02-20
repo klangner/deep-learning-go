32848ec812178a4567b19993747df33ddbe92c32
Krzysztof Langner <krzysztof.langner@img.com>
2019-02-20 11:00:17 +0100

    Added parallel execution to bot vs bot

--- gomoku/bot_v_bot.py
+++ gomoku/bot_v_bot.py
@@ -1,15 +1,14 @@
-from gomoku.agent import RandomBot
-from gomoku import ruleset
-from gomoku.utils import print_board, print_move
 import time
+import argparse
+import multiprocessing
+
+from gomoku.agent import RandomBot
+from gomoku.ruleset import Player, GameState
+from gomoku.utils import print_board, print_move
 
 
-def play_slow(board_size: int):
-    game = ruleset.GameState.new_game(board_size)
-    bots = {
-        ruleset.Player.black: RandomBot(),
-        ruleset.Player.white: RandomBot(),
-    } 
+def play_slow(board_size: int, bots) -> Player:
+    game = GameState.new_game(board_size)
     while not game.is_over():
         time.sleep(0.2)  
 
@@ -24,23 +23,58 @@ def play_slow(board_size: int):
         print('Draw')
     else:
         print('{} won the game'.format(winner))
+    return winner
 
-def play_fast(board_size: int):
+
+def play_fast(board_size: int, bots) -> Player:
     """This can play 4.7 games/sec on 19x19 on MacBook Air"""
-    game = ruleset.GameState.new_game(board_size)
-    bots = {
-        ruleset.Player.black: RandomBot(),
-        ruleset.Player.white: RandomBot(),
-    } 
+    game = GameState.new_game(board_size)
     while not game.is_over():
         bot_move = bots[game.next_player].select_move(game)
         game = game.apply_move(bot_move)
+    return game.winner()
+
+
+def play_games(num_games: int, board_size: int, bots: dict):
+    winners = []
+    for _ in range(num_games):
+        winner = play_fast(board_size, bots)
+        winners.append(winner)
+    return winners
+
+
+def play_parallel(num_games: int, board_size: int, bots: dict):
+    cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=cores)
+    winners = pool.starmap(play_fast, [(board_size, bots) for _ in range(num_games)])
+    return winners
+
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-games', '-n', type=int, default=100)
+    parser.add_argument('--board-size', '-b', type=int, default=19)
+    args = parser.parse_args()
+
+    # Prepare bots
+    bots = {
+        Player.black: RandomBot(),
+        Player.white: RandomBot(),
+    } 
+
+    # Run games
+    start_time = time.time()
+    winners = play_parallel(args.num_games, args.board_size, bots)
+    total_time = time.time() - start_time
+
+    # Print result
+    games_per_second = args.num_games / total_time
+    print("Running {} games in {:.2f}s {:.2f} games/sec".format(args.num_games, total_time, games_per_second))
+    black_wins = len([x for x in winners if x == Player.black])
+    white_wins = len([x for x in winners if x == Player.white])
+    print("Black wins: {}, White wins: {}".format(black_wins, white_wins))
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    ngames = 100
-    for _ in range(ngames):
-        play_fast(19)
-    total_time = time.time() - start_time
-    print("Running {} games in {:.2f}s {:.2} games/sec".format(ngames, total_time, ngames/total_time))
+    main()
